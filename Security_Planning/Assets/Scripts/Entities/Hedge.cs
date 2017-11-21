93871ee9abcf21ef3c987ad012cae7e719d52f4e
Kerzak <lubomir.ohman@yahoo.com>
2017-11-21 23:12:17 +0100

    Hedges introduced.

---
+++ Security_Planning/Assets/Scripts/Entities/Hedge.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Entities
+{
+    public class Hedge : BaseObject
+    {
+        protected override void Start()
+        {
+            base.Start();
+            Vector3 position = transform.position;
+            transform.position = new Vector3(position.x, position.y, -1);
+        }
+    }
+}
